a5e237e6723da7c4c3eae21204c14d16f0a2ee4d
aliii390 <alli.said.errahmani@gmail.com>
2024-11-18 16:39:16 +0100

    ADD: debut du 1er exo bonus

---
+++ script.js
@@ -0,0 +1,15 @@
+const color = ["red", "blue", "yellow", "purple", "pink"];
+
+const box = document.querySelectorAll("div");
+
+
+box.forEach((div) => {
+    div.addEventListner("click", handleClick);
+})
+
+
+
+
+function handleClick(){
+    
+}
