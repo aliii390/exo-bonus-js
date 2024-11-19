72418f697f05762718a72e4dd115f94379a1c50a
aliii390 <alli.said.errahmani@gmail.com>
2024-11-19 15:02:46 +0100

    EXO5

---
+++ exo5/script.js
@@ -0,0 +1,15 @@
+const next = document.getElementById("apres");
+const avant = document.getElementById("avant");
+
+const tableauImage = ["./image/image2.jpg", "./image/image3.jpg"]
+
+const image = document.getElementById("image");
+
+
+next.addEventListener("click", handleClick);
+
+function handleClick(){
+    image.src[0]
+
+}
+
