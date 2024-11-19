94375e1adfa0de0150877e78787a9267ecf0774c
aliii390 <alli.said.errahmani@gmail.com>
2024-11-19 15:11:05 +0100

    ADD: fin du caroussel

--- exo5/script.js
+++ exo5/script.js
@@ -1,15 +1,33 @@
-const next = document.getElementById("apres");
-const avant = document.getElementById("avant");
+const track = document.querySelector('.carousel-track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel-button.right');
+const prevButton = document.querySelector('.carousel-button.left');
+const dots = Array.from(document.querySelectorAll('.carousel-indicator'));
 
-const tableauImage = ["./image/image2.jpg", "./image/image3.jpg"]
+let currentIndex = 0;
 
-const image = document.getElementById("image");
+const updateCarousel = (index) => {
+  track.style.transform = `translateX(-${index * 100}%)`;
+  document.querySelector('.carousel-indicator.current-slide').classList.remove('current-slide');
+  dots[index].classList.add('current-slide');
+};
 
 
-next.addEventListener("click", handleClick);
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  updateCarousel(currentIndex);
+});
 
-function handleClick(){
-    image.src[0]
 
-}
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  updateCarousel(currentIndex);
+});
 
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    currentIndex = index;
+    updateCarousel(currentIndex);
+  });
+});
